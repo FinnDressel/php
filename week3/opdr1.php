96fbf9e819b3a780c072093a30be3e1bfc8b8ef2
FinnDressel <finn.dressel@gmail.com>
2022-06-08 10:15:02 +0200

    php

--- week3/opdr1.php
+++ week3/opdr1.php
@@ -4,3 +4,5 @@ $j = 10;
 for($i = 1; $i <= $j; $i++){
     echo "<img src = 'IMG/".$i.".jpg'>";
 }
+
+
--- week3/opdr2.php
+++ week3/opdr2.php
@@ -1,1 +1,13 @@
 <?php
+echo "<div style='text-align: center'>";
+$i = "*";
+
+for($j = 0; $j <= 9; $j++){
+    for($k = 0; $k <= $j; $k++){
+        echo $i;
+    }
+    echo "<br>";
+}
+echo "</div>";
+?>
+
