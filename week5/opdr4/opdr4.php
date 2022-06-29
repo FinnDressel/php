d37ee5ce10c14f0111edf1a5e78fca272bbeb3f3
FinnDressel <finn.dressel@gmail.com>
2022-06-29 09:21:33 +0200

    php

--- week5/opdr4/opdr4.php
+++ week5/opdr4/opdr4.php
@@ -1,1 +1,22 @@
 <?php
+
+$check = 0;
+$gegevens = array(
+    array("piet@worldonline.nl", "doetje123"),
+    array("klaas@carpets.nl", "snoepje777"),
+    array("truushendriks@wegweg.nl", "arkiearkie201"));
+
+$email = $_POST["email"];
+$wachtwoord = $_POST["Wachtwoord"];
+
+for($i = 0; $i < 3; $i++){
+    if($email == $gegevens[$i][0] && $wachtwoord == $gegevens[$i][1]){
+        $check = 1;
+    }
+}
+if($check == 1){
+    echo "Welkom";
+} else{
+    echo "Sorry geen toegang";
+}
+
